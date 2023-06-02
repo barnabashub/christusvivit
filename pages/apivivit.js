4dac8fe7d71ad2c224cb6580755cc7a02640f77d
barnabashub <66264092+barnabashub@users.noreply.github.com>
2023-06-03 01:52:19 +0200

    Update apivivit.js

--- pages/apivivit.js
+++ pages/apivivit.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { Radio, Text, Spacer } from "@nextui-org/react";
 
 export default function ApiTest() {
   const [apiResponse, setApiResponse] = useState('');
@@ -11,29 +10,11 @@ export default function ApiTest() {
         setApiResponse(data);
       });
   }, []);
-  
-    const [checked, setChecked] = React.useState('');
 
   return (
     <div>
       <h1>API Test</h1>
       <p>API response: {apiResponse}</p>
-<h3>asd</h3>
     </div>
-
-    <>
-      <Radio.Group 
-        label="Options"
-        value={checked}
-        onChange={setChecked}
-      >
-        <Radio value="A">Option A</Radio>
-        <Radio value="B">Option B</Radio>
-        <Radio value="C">Option C</Radio>
-        <Radio value="D">Option D</Radio>
-      </Radio.Group>
-      <Spacer y={1} />
-      <Text>You've checked: {checked}</Text>
-    </>
   );
 }
